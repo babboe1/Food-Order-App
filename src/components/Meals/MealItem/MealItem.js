c1b99c565d5a091aca6e3c3b09d4f378b2f254f0
babboeCodes <babboecodes@gmail.com>
2022-11-17 20:46:21 +0100

    refactor: add className to list

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import MealItemForm from '../MealItemForm/MealItemForm';
 import classes from './MealItem.module.css';
 
 const MealItem = (props) => {
    return (
-      <li>
-         <div className={classes.meal}>
-            <h3>{ props.name }</h3>
-            <div className={classes.title}>{ props.title }</div>
-            <div className={classes.price}>{`$${ props.price.toFixed(2)}` }</div>
+      <li className={classes.meal}>
+         <div>
+            <h3>{props.name}</h3>
+            <div className={classes.title}>{props.title}</div>
+            <div className={classes.price}>{`$${props.price.toFixed(2)}`}</div>
+         </div>
+         <div>
+            <MealItemForm />
          </div>
       </li>
    );
