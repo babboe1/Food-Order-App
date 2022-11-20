369f8cc77e2200f838d14b43c862f0129c27bb31
babboeCodes <babboecodes@gmail.com>
2022-11-20 17:24:19 +0100

    update cart component

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Modal from '../UI/Modal/Modal';
 import classes from './Cart.module.css';
 
 const Cart = () => {
@@ -11,17 +12,17 @@ const Cart = () => {
    );
 
    return (
-      <>
+      <Modal>
          {cartItems}
          <div className={classes.total}>
             <span>Total Amount</span>
             <span>35.62</span>
          </div>
-         <div>
+         <div className={classes.actions}>
             <button className={classes['button--alt']}> Close</button>
             <button className={classes['button']}>Order</button>
          </div>
-      </>
+      </Modal>
    );
 };
 
