0445e386dc11ee9f94a6c9082eb5fc6970e1bf28
babboeCodes <babboecodes@gmail.com>
2022-11-21 21:59:12 +0100

    manage State for modal and Backdrop - create useState function to show modal and backdrop - create onclick handler to change state

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Cart from './components/Cart/Cart';
 import Header from './components/Layout/Header/Header';
 import Meals from './components/Meals/Meals';
 
 const App = () => {
+   const [ showModal, setShowModal ] = useState( false );
+   const showModalHandler=  (prev) => setShowModal( !prev );
+
+
    return (
       <>
          <Header />
-         <Cart />
+         {showModal ? <Cart /> : null}
          <main>
             <Meals />
          </main>
