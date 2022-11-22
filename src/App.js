a411498adf43b7fa20f54583f913afd39322f96d
babboeCodes <babboecodes@gmail.com>
2022-11-22 09:26:58 +0100

    App component update - add Context Api - create modal state - create DOM element

--- src/App.js
+++ src/App.js
@@ -1,21 +1,38 @@
 import React, { useState } from 'react';
 import Cart from './components/Cart/Cart';
+import Context from './Store/ContextApi/Context';
 import Header from './components/Layout/Header/Header';
 import Meals from './components/Meals/Meals';
+import CartProvider from './Store/CartContext/CartProvider';
 
 const App = () => {
-   const [ showModal, setShowModal ] = useState( false );
-   const showModalHandler=  (prev) => setShowModal( !prev );
+   const body = document.getElementById('body-root');
+   const [showModal, setShowModal] = useState(false);
+   const showModalHandler = () => {
+      setShowModal( true );
+      body.classList.add('Stop-Scroll');
+   };
+   const hideModalHandler = () => {
+      setShowModal( false );
+      body.classList.remove('Stop-Scroll');
+   };
 
+   const value = {
+      showModal,
+      showModalHandler,
+      hideModalHandler,
+   };
 
    return (
-      <>
-         <Header />
-         {showModal ? <Cart /> : null}
-         <main>
-            <Meals />
-         </main>
-      </>
+      <Context.Provider value={value}>
+         <CartProvider>
+            <Header onShowCart={showModalHandler} />
+            {showModal ? <Cart /> : null}
+            <main>
+               <Meals />
+            </main>
+         </CartProvider>
+      </Context.Provider>
    );
 };
 
