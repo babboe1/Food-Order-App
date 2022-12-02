11589ab7d1de0f8808c41d6b4b3f45f0c224f5f8
babboeCodes <babboecodes@gmail.com>
2022-12-02 13:08:46 +0100

    feat: add meal fom to select meal to add to cart - create submit handler function for form on submission - create state to set valid amount of item

--- src/components/Meals/MealItemForm/MealItemForm.js
+++ src/components/Meals/MealItemForm/MealItemForm.js
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import Input from '../../UI/Input/Input';
 import classes from './MealItemForm.module.css';
 
-const MealItemForm = () => {
+const MealItemForm = (props) => {
+   const [amountIsValid, setAmountIsValid] = useState(true);
+   const amountInputRef = useRef();
+   const submitHandler = (event) => {
+      event.preventDefault();
+      const enteredAmount = amountInputRef.current.value;
+      const enteredAmountNumber = +enteredAmount;
+
+      if (
+         enteredAmount.trim().length === 0 ||
+         enteredAmountNumber < 1 ||
+         enteredAmountNumber > 5
+      ) {
+         setAmountIsValid(false);
+         return;
+      }
+      setAmountIsValid(true);
+      props.onAddToCart(enteredAmountNumber);
+   };
+
    return (
-      <form className={classes.form}>
+      <form className={classes.form} onSubmit={submitHandler}>
          <Input
+            ref={amountInputRef}
             label="amount"
             input={{
-               id: 'amount',
+               id: props.id,
                type: 'number',
                min: 1,
                max: 5,
@@ -17,6 +37,7 @@ const MealItemForm = () => {
             }}
          />
          <button>+ Add</button>
+         {!amountIsValid && <p>Please enter a valid amount (1-5).</p>}
       </form>
    );
 };
