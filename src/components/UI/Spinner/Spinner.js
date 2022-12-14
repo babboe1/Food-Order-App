aaa19d268fea49c5c7994e7c1bde5f70257bdf48
babboeCodes <babboecodes@gmail.com>
2022-12-14 09:01:54 +0100

    update loader component

--- src/components/UI/Spinner/Spinner.js
+++ src/components/UI/Spinner/Spinner.js
@@ -1,5 +1,17 @@
 import React from 'react';
 import classes from './spinner.module.css';
 
+const Spinner = () => {
+   return (
+      <>
+         <div className={classes.loader}>Loading...</div>
+         {(() => {
+            setTimeout(() => {
+               return <p>Grab cup of coffee...</p>;
+            }, 10000);
+         })()}
+      </>
+   );
+};
 
 export default Spinner;
