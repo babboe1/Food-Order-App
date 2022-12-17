a89261e80e4392b41ca60d2def93f8109decb92f
babboeCodes <babboecodes@gmail.com>
2022-12-17 09:06:27 +0100

    update component

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import CartContext from '../../Store/CartContext/CartContext';
 import Context from '../../Store/ContextApi/Context';
 import Modal from '../UI/Modal/Modal';
