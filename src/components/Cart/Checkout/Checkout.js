eeff82c3ea64c7726fab97685d6245d2c37db80d
babboeCodes <babboecodes@gmail.com>
2022-12-24 09:14:27 +0100

    create state for form validity

--- src/components/Cart/Checkout/Checkout.js
+++ src/components/Cart/Checkout/Checkout.js
@@ -4,6 +4,14 @@ import classes from './Checkout.module.css';
 const isEmpty = (value) => value.trim() === '';
 const isFiveChars = (value) => value.trim().length === 5;
 
+const Checkout = (props) => {
+   const [formInputsValidity, setFormInputsValidity] = useState({
+      name: true,
+      street: true,
+      postal: true,
+      city: true,
+   });
+
 
 
 export default Checkout;
