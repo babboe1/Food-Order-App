8a4691551f3972ae4c1ff9982bef3451d90507d6
babboeCodes <babboecodes@gmail.com>
2022-12-24 09:13:41 +0100

    create condition constant

--- src/components/Cart/Checkout/Checkout.js
+++ src/components/Cart/Checkout/Checkout.js
@@ -1,5 +1,9 @@
 import React, { useRef, useState } from 'react';
 import classes from './Checkout.module.css';
 
+const isEmpty = (value) => value.trim() === '';
+const isFiveChars = (value) => value.trim().length === 5;
+
+
 
 export default Checkout;
