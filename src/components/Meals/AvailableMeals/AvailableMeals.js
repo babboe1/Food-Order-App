d61da6d8a316eea44dbc2e99d2ee2112ce749a75
babboeCodes <babboecodes@gmail.com>
2022-12-24 09:24:45 +0100

    create default state object

--- src/components/Meals/AvailableMeals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals/AvailableMeals.js
@@ -5,32 +5,14 @@ import Spinner from '../../UI/Spinner/Spinner';
 import MealItem from '../MealItem/MealItem';
 import classes from './AvailableMeals.module.css';
 
-const DUMMY_MEALS = [
-   {
-      id: 'm1',
-      name: 'Sushi',
-      description: 'Finest fish and veggies',
-      price: 22.99,
-   },
-   {
-      id: 'm2',
-      name: 'Schnitzel',
-      description: 'A german specialty!',
-      price: 16.5,
-   },
-   {
-      id: 'm3',
-      name: 'Barbecue Burger',
-      description: 'American, raw, meaty',
-      price: 12.99,
-   },
-   {
-      id: 'm4',
-      name: 'Green Bowl',
-      description: 'Healthy...and green...',
-      price: 18.99,
-   },
-];
+const AvailableMeals = () => {
+   const defaultMealState = {
+      meals: null,
+      error: false,
+      errorMsg: '',
+   };
+ 
+
 
    const mealsList = state.meals ? (
       state.meals.map((meal, idx) => (
