c01f05f44ee36f34a40b35f2d030e5f0e9591b70
babboeCodes <babboecodes@gmail.com>
2023-01-01 09:32:39 +0100

    feat: add header cart button - create header cart component - set and clear time out to animate button on click

--- src/components/Layout/Header/HeaderCartBtn/HeaderCartBtn.js
+++ src/components/Layout/Header/HeaderCartBtn/HeaderCartBtn.js
@@ -1,13 +1,37 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import classes from './HeaderCartBtn.module.css';
 import CartIcon from '../../../Cart/CartIcon/CartIcon';
+import CartContext from '../../../../Store/CartContext/CartContext';
 
-const HeaderCartBtn = () => {
+const HeaderCartBtn = (props) => {
+   const context = useContext(CartContext);
+   const cartItems = context.cartLength;
+
+   const btnClasses = `${ classes.button } ${ context.bump ? classes.bump : '' }`;
+
+   useEffect(() => {
+      if (cartItems === 0) {
+         return;
+      }
+      context.setBump( true );
+      const timer = setTimeout(() => {
+         context.setBump( false );
+         
+      }, 300);
+      return () => {
+         clearTimeout(timer);
+      };
+   // eslint-disable-next-line react-hooks/exhaustive-deps
+   }, [context.items]);
+   
+
    return (
-      <button className={classes.button}>
-         <span className={classes.icon}><CartIcon /></span>
+      <button onClick={props.click} className={btnClasses}>
+         <span className={classes.icon}>
+            <CartIcon />
+         </span>
          <span>Your Cart</span>
-         <span className={classes.badge}>3</span>
+         <span className={classes.badge}>{cartItems}</span>
       </button>
    );
 };
